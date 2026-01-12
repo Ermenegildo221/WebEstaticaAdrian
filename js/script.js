a83215ed63ffed245d8443a4a4803e05a4604aa7
Ermenegildo221 <adriannxd10x@gmail.com>
2026-01-12 13:27:39 +0100

    cambios practica 4-5

--- js/script.js
+++ js/script.js
@@ -1,25 +1,13 @@
-    document.addEventListener('DOMContentLoaded', () => {
-      // Referencias
-      const boton = document.getElementById('boton');
-      const mensaje = document.getElementById('mensaje');
+document.addEventListener('DOMContentLoaded', () => {
+  // Referencias
+  const boton = document.forms["formulario"];
+  const mensaje = document.getElementById("mensaje");
+  
 
-      // Textos que vamos rotando
-      const textos = [
-        'Bienvenido a la página de Adrián',
-        '¡has pulsado el botón!',
-        'A que ahora si se nota el gym',
-        'Otro mensaje más',
-        'Último mensaje'
-      ];
-
-      // Índice actual — sincronizado con el texto mostrado
-      let indice = 0;
-      // Inicializa la pantalla con el primer texto del array
-      mensaje.textContent = textos[indice];
-
-      // Al hacer click incrementamos el índice y actualizamos el texto
-      boton.addEventListener('click', () => {
-        indice = (indice + 1) % textos.length; // avanza y vuelve al principio
-        mensaje.textContent = textos[indice];
-      });
-    });
+  // Al hacer click incrementamos el índice y actualizamos el texto
+  boton.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const nombre = document.forms["formulario"]["nombre"].value;
+    mensaje.textContent = nombre + " bienvenido a mi página";
+  });
+});
